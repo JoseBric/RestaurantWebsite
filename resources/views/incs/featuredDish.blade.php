4758b54c221a8e1cd8244b98ee19d13a44ed42bc
j-bric <josebric@hotmail.com>
2018-12-02 08:43:52 -0700

    Menu Finished

---
+++ resources/views/incs/featuredDish.blade.php
@@ -0,0 +1,19 @@
+<div class="card dish col-md-4" dish_id="{{ $dish->id }}">
+    <img src="{{ \Storage::disk("s3")->url($dish->image) }}" alt="" class="card-img-top mt-3" style="margin: auto; width: 90%">
+    <div class="card-body">
+        <div class="card-header">
+            <div class="card-title text-center text-capitalize">
+                <h2>{{ $dish->name }}</h2> <input type="checkbox" checked name="featured" class="featuredBox">
+            </div>
+        </div>
+        <br>
+        <div class="card-text text-center description">
+            <h4 class="card-subtitle mb-2 text-muted">Ingredients:</h4>
+            <p>{{ $dish->description }}</p>
+        </div>
+        <div class="text-center">
+            <button class="mr-1 btn btn-danger btn-lg deleteBtn">Delete</button>
+            <a class="btn btn-primary ml-1 btn-lg" href="/dish/{{ $dish->id }}/edit">Update</a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/menu_category.blade.php
@@ -0,0 +1,23 @@
+@extends("layouts.app")
+@section("id", "menu_category")
+@section('content')
+    <div class="container">
+        <div class="dishes-menu">
+            @foreach ($dishes as $dish)
+                @php
+                $image = \Storage::disk("s3")->url($dish->image);
+                @endphp
+                <div class="dish">
+                    <h3><span>{{ $dish->name }}</span></h3>
+                    <div class="img" style="background: url({{ $image }}); background-size: cover;"></div>
+                    <div class="ingredients">
+                        <h4>Ingredients:</h4>
+                        <p>
+                            {{ $dish->description }}
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
